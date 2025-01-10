0769172bb94eb95b577b9830ed70c8440d47c1de
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2025-01-10 14:56:32 +0800

    created search functions for specific and generic search

--- RefineSearchPage.js
+++ RefineSearchPage.js
@@ -1,75 +1,94 @@
 const byTopicSearchBtn = document.getElementById("byTopicSearchBtn");
 const byCategorySearchBtn = document.getElementById("byCategorySearchBtn");
-// Topic dropdown menu
-const dropdownTopicItems = document.querySelectorAll(".dropdown-menuTopic .dropdown-itemTopic");
-const dropdownCategoryItems = document.querySelectorAll(".dropdown-menuCategory .dropdown-itemCategory");
-
-
-searchInputTopic.value
 
 /*
  * Search by Topic 
  */
 
 // Search by topic button
-byTopicSearchBtn.addEventListener("click", () => {
-
-    const topicSearchTerm = document.getElementById("searchInputTopic").value.trim();
-    const selectedTopic = document.querySelector(".dropdown-menuTopic .active");
-    const selectedDropdownName = selectedTopic ? selectedTopic.textContent.trim() : null;
-
-    if(topicSearchTerm && selectedDropdownName){
-        const pageURL = new URL(window.location.origin);
-        const searchURL = `${pageURL}?search=1query=${(topicSearchTerm)}&topic=${(selectedDropdownName)}`;
-
-        window.location.href = searchURL;
-    }else{
-        alert("Please enter a search term and select a topic!!");
-    }
+byTopicSearchBtn.addEventListener("click", (event) => {
+    return performSearch(event.target.id);
 })
 
-// Topic Dropdown
-dropdownTopicItems.forEach(item => {
-    item.addEventListener("click", () => {
-        dropdownTopicItems.forEach(item => item.classList.remove("active"));
-        item.classList.add("active");
-
-        const dropdownButton = document.getElementById("dropdownMenuButtonTopic");
-        dropdownButton.textContent = item.textContent;
-    });
-});
-
-
 /*
- * Search by category
+ * Search by Category 
  */
 
 // Search by category button
 byCategorySearchBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const categorySearchTerm = document.getElementById("searchInputCategory").value.trim();
-    const selectedCategory = document.querySelector(".dropdown-menuCategory .active");
-    const selectedDropdownName = selectedCategory ? selectedCategory.textContent.trim() : null;
-
-    if(categorySearchTerm && selectedDropdownName){
-        const pageURL = new URL(window.location.origin);
-        const searchURL = `${pageURL}?search=1query=${(categorySearchTerm)}&topic=${(selectedDropdownName)}`;
-
-        window.location.href = searchURL;
-    }else{
-        alert("Please enter a search term and select a category!!");
-    }
-
+    return performSearch(event.target.id);
 })
 
-// Category dropdown
-dropdownCategoryItems.forEach(item => {
-    item.addEventListener("click", () => {
-        dropdownCategoryItems.forEach(item => item.classList.remove("active"));
-        item.classList.add("active");
+function performSearch(inputTypeId = null){
 
-        const dropdownButton = document.getElementById("dropdownMenuButtonCategory");
-        dropdownButton.textContent = item.textContent;
+    let searchType = "";
+    let dropdownType = "";
+    let typeOfId = "";
+    let searchMethod = 1;
+    let resultsPage = "index.html";
+
+    if(inputTypeId === "byTopicSearchBtn"){
+        searchType = "searchInputTopic";
+        dropdownType = "topicDropdown";
+        typeOfId = "topic_id";
+    }else if(inputTypeId === "byCategorySearchBtn"){
+        searchType = "searchInputCategory";
+        dropdownType = "categoryDropdown";
+        typeOfId = "category_id";
+    }else{
+        // TODO Generic Search implementation
+        searchMethod = 2;
+    }
+
+    const searchTerm = document.getElementById(searchType).value.trim();
+    const dropDown = document.getElementById(dropdownType);
+    const selectedDropdownId = dropDown ? dropDown.options[dropDown.selectedIndex].value : null;
+
+    if(searchTerm && selectedDropdownId){
+
+        let fullURL = `${window.location.origin}/${resultsPage}`;
+
+        const pageURL = new URL(fullURL);
+
+        if(searchMethod === 1)
+            // specific Search refers to topic and category search
+            window.location.href = `${pageURL}?search=${searchMethod}&query=${(searchTerm)}&${typeOfId}=${(selectedDropdownId)}`;
+        else
+            // otherwise it is a generic search 
+            window.location.href = `${pageURL}?search=${searchMethod}&query=${(searchTerm)}`;
+
+    }else{
+        alert("Please enter a search term and select a topic!!");
+    }
+}
+
+// Only when the page is loaded
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    // Topic dropdown menu
+    const dropDownTopicItems = [
+        {id: 1, name: "Tips"},
+        {id: 2, name: "Plant Exchange"},
+        {id: 3, name: "Giveaway"},
+    ];
+
+    // Category dropdown menu
+    const dropDownCatogeryItems = [
+        {id: 1, name: "Tips"},
+        {id: 2, name: "Plant Exchange"},
+        {id: 3, name: "Giveaway"},
+    ];
+  
+    // Topic Dropdown
+    dropDownTopicItems.forEach(item => {
+       // TODO add the option(s) textContent and value
+       // target: topicDropdown
     });
+
+    // Category dropdown
+    dropDownCatogeryItems.forEach(item => {
+        // TODO add the option(s) textContent and value
+        // target: categoryDropdown
+    });
+
 });
