b771c239e6588959a32ad65d533cd50770f97653
ChanJESG <chanjiaenspore@gmail.com>
2025-02-03 20:02:54 +0800

    modified create-post-dropdown to use backend api

--- script/create-post-dropdown.js
+++ script/create-post-dropdown.js
@@ -1,40 +1,15 @@
-function topicOptions() {
-    // TODO add in similar try catch from comments
-    return new Array("Showcase", "Help", "Q&A", "Tips", "Exchange", "Giveaway");
-};
-
-function categoryOptions() {
-    // TODO add in similar try catch from comments
-    return new Array("Succulents", "Leafy plants", "Creepers", "Moss");
-};
-
-const topicArr = topicOptions();
-const categoryArr = categoryOptions();
-
 const topicList = document.getElementById("topic-dropdown");
 const categoryList = document.getElementById("category-dropdown");
 
-for (let index = 0; index < topicArr.length; index++) {
-    topicList.options[index + 1] = new Option(topicArr[index], index + 1);
-}
-
-for (let index = 0; index < categoryArr.length; index++) {
-    categoryList.options[index + 1] = new Option(categoryArr[index], index + 1);
-}
-
-/* async function fetchCategories() {
+async function fetchCategories() {
     
     try {
-        const response = await fetch('https://perenual.com/api/species-list?key=sk-AWTM6785252daddf38215');
+        const response = await fetch(_PUBLIC_ENDPOINT_CATEGORY);
         const result = await response.json();
 
-        const categoryOptions = result.data;
-        console.log(categoryOptions[1].id);
-
-        for (let index = 0; index <= 10; index++) {
-            categoryList.options[index + 1] = new Option(categoryOptions[index].scientific_name, categoryOptions[index].id);
+        for (let index = 0; index < result.length; index++) {
+            categoryList.options[index + 1] = new Option(result[index].name, result[index].id);
         }
-
         
     } catch (error) {
         alert ("Error: " + error)
@@ -42,4 +17,21 @@ for (let index = 0; index < categoryArr.length; index++) {
     }
 }
 
-fetchCategories(); */
+async function fetchTopics() {
+    
+    try {
+        const response = await fetch(_PUBLIC_ENDPOINT_TOPIC);
+        const result = await response.json();
+
+        for (let index = 0; index < result.length; index++) {
+            topicList.options[index + 1] = new Option(result[index].name, result[index].id);
+        }
+
+    } catch (error) {
+        alert ("Error: " + error)
+        
+    }
+}
+
+fetchCategories();
+fetchTopics();
